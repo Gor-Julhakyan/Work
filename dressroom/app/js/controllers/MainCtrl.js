1adf23b6772d7992403e4b9d619ebd7cbbe0fe46
Gor Julhakyan <gorjulhakyan90@gmail.com>
2016-01-29 17:13:21 +0400

    ---

--- dressroom/app/js/controllers/MainCtrl.js
+++ dressroom/app/js/controllers/MainCtrl.js
@@ -1,5 +1,7 @@
 app.controller('MainCtrl', ['$scope', function($scope) {
 	$scope.stageOne = true;
+	$scope.enterFemale = false;
+	$scope.enterMale = false;
 
 	$scope.genders = [
 		{
@@ -21,15 +23,42 @@ app.controller('MainCtrl', ['$scope', function($scope) {
 		switch(stage) {
 	      case "male":
 	         $scope.stageOne = false;
+	         $scope.models[0].action = true;
 	         break;
 	      case "female":
 	         $scope.stageOne = false;
+	         $scope.models[1].action = true;
 	         break;
 	      default:
-	         console.log('There should not be a default failover at this thing.');
-   }
+   		}
+	};
+
+	$scope.models = [
+		{
+			action: false,
+			class: "male-enter",
+			items: [
+				{ model: "assets/img/models/boy_1.png" },
+				{ model: "assets/img/models/boy_2.png" },
+				{ model: "assets/img/models/boy_3.png" }
+			]
+		},
+		{
+			action: false,
+			class: "female-enter",
+			items: [
+				{ model: "assets/img/models/girl_1.png" },
+				{ model: "assets/img/models/girl_2.png" },
+				{ model: "assets/img/models/girl_3.png" }
+			]
+		}
+	];
+	
+	$scope.activeModel = "";
+
+	$scope.isActive = function (src) {
+		$scope.activeModel = src;
+		console.log($scope.activeModel);
 	}
-
-
-
+	
 }]);
