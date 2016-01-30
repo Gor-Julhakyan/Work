67baa1be77afc5bee9c98b83beb880be9d2e39f4
Gor-Julhakyan <gorjulhakyan90@gmail.com>
2016-01-30 04:59:18 +0400

    ....

--- dressroom/app/js/controllers/MainCtrl.js
+++ dressroom/app/js/controllers/MainCtrl.js
@@ -1,7 +1,8 @@
 app.controller('MainCtrl', ['$scope', function($scope) {
-	$scope.stageOne = true;
+	$scope.stageOne = false;
 	$scope.enterFemale = false;
 	$scope.enterMale = false;
+	$scope.clothesFilter = "";
 
 	$scope.genders = [
 		{
@@ -24,10 +25,12 @@ app.controller('MainCtrl', ['$scope', function($scope) {
 	      case "male":
 	         $scope.stageOne = false;
 	         $scope.models[0].action = true;
+	         $scope.clothesFilter = "male";
 	         break;
 	      case "female":
 	         $scope.stageOne = false;
 	         $scope.models[1].action = true;
+	         $scope.clothesFilter = "female";
 	         break;
 	      default:
    		}
@@ -35,7 +38,7 @@ app.controller('MainCtrl', ['$scope', function($scope) {
 
 	$scope.models = [
 		{
-			action: false,
+			action: true,
 			class: "male-enter",
 			items: [
 				{ model: "assets/img/models/boy_1.png" },
@@ -61,4 +64,60 @@ app.controller('MainCtrl', ['$scope', function($scope) {
 		console.log($scope.activeModel);
 	}
 	
+	$scope.clothes = [
+		{
+			category: "t-shirt",
+			items: [
+				{ img:"assets/img/clothes/tshirts/tshirt_1.png", filter: "male" },
+				{ img:"assets/img/clothes/tshirts/tshirt_2.png", filter: "male" },
+				{ img:"assets/img/clothes/tshirts/tshirt_3.png", filter: "female" },
+				{ img:"assets/img/clothes/tshirts/tshirt_4.png", filter: "male" },
+				{ img:"assets/img/clothes/tshirts/tshirt_5.png", filter: "male" },
+				{ img:"assets/img/clothes/tshirts/tshirt_6.png", filter: "male" },
+				{ img:"assets/img/clothes/tshirts/tshirt_7.png", filter: "male" },
+				{ img:"assets/img/clothes/tshirts/tshirt_8.png", filter: "male" },
+				{ img:"assets/img/clothes/tshirts/tshirt_9.png", filter: "female" },
+				{ img:"assets/img/clothes/tshirts/tshirt_10.png", filter: "female" },
+				{ img:"assets/img/clothes/tshirts/tshirt_11.png", filter: "female" }				
+			]
+		},	
+		{
+			category: "trousers",
+			items: [
+				{ img: "assets/img/clothes/jeans/jeans_1.png", filter: "male" },
+				{ img: "assets/img/clothes/jeans/jeans_2.png", filter: "male" },
+				{ img: "assets/img/clothes/jeans/jeans_3.png", filter: "female" },
+				{ img: "assets/img/clothes/jeans/jeans_4.png", filter: "female" },
+				{ img: "assets/img/clothes/jeans/jeans_5.png", filter: "female" }				
+			]
+		},
+		{
+			category: "dresses",
+			items: [
+				{ img: "assets/img/clothes/dresses/dress_1.png", filter: "female" },
+				{ img: "assets/img/clothes/dresses/dress_2.png", filter: "female" },
+				{ img: "assets/img/clothes/dresses/dress_3.png", filter: "female" },
+				{ img: "assets/img/clothes/dresses/dress_4.png", filter: "female" }				
+			]
+		},
+		{
+			category: "",
+			items: [
+				
+			]
+		},
+		{
+			category: "",
+			items: [
+				
+			]
+		}	
+	];
+
+	$scope.tshirt = "";
+
+	$scope.msover = function (img) {
+		$scope.tshirt = img; 
+	}
+
 }]);
